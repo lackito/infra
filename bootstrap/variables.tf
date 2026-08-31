2cafe72863a666d300c53d733392b188ad7918d6
lackito <sergio.benites@gmail.com>
2026-08-30 22:56:53 -0400

    Add bootstrap - terraform remote state S3 bucket

---
+++ bootstrap/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket to store Terraform state"
+  type        = string
+}
+
+  
